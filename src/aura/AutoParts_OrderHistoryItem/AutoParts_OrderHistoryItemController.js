d4c2360fa5ad7819f159f519d014409f1b086aee
PiterT <ptoporowski@gmail.com>
2019-03-29 09:14:34 +0100

    12 / Orders' history / Add orders' history

---
+++ src/aura/AutoParts_OrderHistoryItem/AutoParts_OrderHistoryItemController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by BRITENET on 27.03.2019.
+ */
+({
+   init: function(component, event, helper) {
+       console.log('init');
+       helper.doInit(component, event);
+   },
+
+})
