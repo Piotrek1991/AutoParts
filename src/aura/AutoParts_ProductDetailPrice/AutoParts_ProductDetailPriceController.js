a43e59427567cec245c9341c19e6b60661f0e502
PiterT <ptoporowski@gmail.com>
2019-04-02 13:27:12 +0200

    10 / Product details / Refactor

--- src/aura/AutoParts_ProductDetailPrice/AutoParts_ProductDetailPriceController.js
+++ src/aura/AutoParts_ProductDetailPrice/AutoParts_ProductDetailPriceController.js
@@ -3,23 +3,24 @@
  */
 ({
     init: function(component, event, helper) {
-        helper.doInit(component, event);
+        helper.doInit(component);
     },
 
     clickButtonDown: function(component, event, helper) {
         let amount = component.get("v.amountProduct");
         if(amount > 1){
-            helper.doButtonAmount(component, event, -1);
+            helper.doButtonAmount(component, -1);
         }
     },
 
     clickButtonUp: function(component, event, helper) {
         let amount = component.get("v.amountProduct");
         if(amount < 100){
-            helper.doButtonAmount(component, event, 1);
+            helper.doButtonAmount(component, 1);
         }
     },
+
     clickAddToBasket: function(component, event, helper) {
-        helper.doAddToBasket(component, event);
+        helper.doAddToBasket(component);
     },
 })
