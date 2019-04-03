2f6cbbe07c11f87c8ca900874624bf18017b7262
PiterT <ptoporowski@gmail.com>
2019-04-03 08:10:07 +0200

    10 / Product details / Refactor 2

--- src/aura/AutoParts_ProductDetail/AutoParts_ProductDetailHelper.js
+++ src/aura/AutoParts_ProductDetail/AutoParts_ProductDetailHelper.js
@@ -2,9 +2,8 @@
  * Created by BRITENET on 19.03.2019.
  */
 ({
-    doInit: function(component) {
-        let productId = component.get("v.recordId");
-        component.set("v.productId", productId);
+    doInit: function(component, event) {
+        let productId = sessionStorage.getItem("showProductDetails--recordIds");
         let action = component.get("c.getProfilePicture");
         action.setParam("parentId", productId);
         action.setCallback(this, function(response) {
@@ -12,20 +11,8 @@
           if (state === 'SUCCESS') {
             let allURL = response.getReturnValue();
             component.set("v.productURL", allURL);
-          } else {
-              this.doShowToast(component, response.getErrors()[0].message, 'Error' , 'Error');
           }
         });
         $A.enqueueAction(action);
-    },
-
-    doShowToast: function(component, message, title, typeToast) {
-        const toastComponent = component.find('toast');
-        if (toastComponent) {
-            toastComponent.showToast(message, title, typeToast);
-        }
-        else {
-            console.error("'Toast Component' does not exist");
-        }
     },
 })
